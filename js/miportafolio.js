a4da75ea2f9fefa79a1627909e0c1dec9122afdf
martin <guerreiromartin@gmail.com>
2022-10-15 12:49:18 -0300

    scroll funcional

--- js/miportafolio.js
+++ js/miportafolio.js
@@ -1,7 +1,16 @@
 window.sr = ScrollReveal();
 
     sr.reveal('.navbar', {
-        duration: 10000,
-        origin: 'left',
-        distance: '-1000'
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
     });
+
+    
+// window.sr = ScrollReveal();
+
+//     sr.reveal('.navbar', {
+//         duration: 3000,
+//         origin: 'bottom',
+//         distance: '-100px'
+//     });
