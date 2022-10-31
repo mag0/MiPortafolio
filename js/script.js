9e4ad704ce1277dab416967f8f7ed41bb41a660f
martin <guerreiromartin@gmail.com>
2022-10-31 01:06:08 -0300

    estructura base

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-window.sr = ScrollReveal();
+// window.sr = ScrollReveal();
 
-    sr.reveal('.navbar', {
-        duration: 3000,
-        origin: 'bottom',
-        distance: '-100px'
-    });
+//     sr.reveal('.navbar', {
+//         duration: 3000,
+//         origin: 'bottom',
+//         distance: '-100px'
+//     });
 
     
 // window.sr = ScrollReveal();
